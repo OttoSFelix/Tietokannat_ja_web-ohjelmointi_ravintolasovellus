9c43967e88e94a689af8852c59ad5a4b836251a5
Omistaja <Omistaja@MacBook-Air.local>
2024-11-21 16:16:24 +0200

    html ja schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users(id SERIAL PRIMARY KEY, name TEXT, password TEXT);
 CREATE TABLE restaurants(id SERIAL PRIMARY KEY, name TEXT UNIQUE, info TEXT, stars_average REAL, created_at TIMESTAMP);
-CREATE TABLE reviews(restaurant_id INTEGER, user_id INTEGER, comment TEXT, stars INTEGER, made_at TIMESTAMP);
+CREATE TABLE reviews(id SERIAL PRIMARY KEY, restaurant_id INTEGER, user_id INTEGER, comment TEXT, stars INTEGER, made_at TIMESTAMP);
 CREATE TABLE groups(id SERIAL PRIMARY KEY, name TEXT);
 CREATE TABLE group_restaurants(group_id INTEGER, restaurant_id INTEGER);
